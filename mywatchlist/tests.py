0fd3c3eab3cad58b7cdea3cedd876a55eb2999ca
arinaaunaka <arinaaunaka@gmail.com>
2022-09-21 16:47:49 +0700

    Tugas 3 - Lab 2

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,16 +1,15 @@
 from django.test import TestCase, Client
-from django.urls import resolve
-from mywatchlist.views import *
+from django.urls import reverse
 
 class TestUrls(TestCase):
     def test_html_url(self):
-        response = Client().get('/mywatchlist/html')
+        response = Client().get(reverse("mywatchlist:show_html"))
         self.assertEquals(response.status_code, 200)
 
     def test_json_url(self):
-        response = Client().get('/mywatchlist/json')
+        response = Client().get(reverse("mywatchlist:show_json"))
         self.assertEquals(response.status_code, 200)
 
     def test_xml_url(self):
-        response = Client().get('/mywatchlist/xml')
+        response = Client().get(reverse("mywatchlist:show_xml"))
         self.assertEquals(response.status_code, 200)
